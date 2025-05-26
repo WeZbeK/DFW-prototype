ebb57c992954684ed67e0e82157f07cc9cc7f1fd
Martinn Admin <martinsbackupmail8@gmail.com>
2025-05-26 20:18:20 +0200

    første upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Rusk IF - Spilleroversigt</h1>
+      <ul>
+        <li>Brian Campbell – Grade: 14.5 – QI: 16.885</li>
+        <li>Patrick Rytter – Grade: 13.6 – QI: 9.437</li>
+        <li>Simone Marzolla – Grade: 13.8 – QI: 13.191</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
